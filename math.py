12191f9e363512f1b967c3134524ce44d4ac2738
Aroop Kumar <aroopkumarofficial@gmail.com>
2021-07-02 14:40:56 +0530

    Implemented add()

--- math.py
+++ math.py
@@ -1,15 +1,23 @@
 # Add implementation
+
+
 def add(x, y):
-    pass
+    return x + y
 
 # Subtract implementation
+
+
 def subtract(x, y):
     pass
 
 # Multiply implementation
+
+
 def multiply(x, y):
     pass
 
 # Divide implementation
+
+
 def divide(x, y):
     pass
